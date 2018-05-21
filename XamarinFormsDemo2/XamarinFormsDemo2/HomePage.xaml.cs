ab299b0fe71e13dd40842534fdb2830531829fd1
Jimmy Chu <jimmy.chu@proqc.com.hk>
2018-05-21 10:35:12 +0800

    Add project files.

---
+++ XamarinFormsDemo2/XamarinFormsDemo2/HomePage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsDemo2
+{
+	public partial class HomePage : ContentPage
+	{
+		public HomePage()
+		{
+			InitializeComponent();
+
+            MainListView.ItemsSource = new List<Person>
+            {
+                new Person
+                {
+                    Name = "Mohamed",
+                    Age = 34
+                },
+                new Person
+                {
+                    Name = "Houssem",
+                    Age = 26
+                },
+                new Person
+                {
+                    Name = "Marwen",
+                    Age = 26
+                },
+                new Person
+                {
+                    Name = "Othman",
+                    Age = 26
+                },
+                new Person
+                {
+                    Name = "Hassen",
+                    Age = 26
+                },
+            };
+		}
+	}
+}
---
+++ XamarinFormsDemo2/XamarinFormsDemo2/MenuPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsDemo2
+{
+
+	public partial class MenuPage : MasterDetailPage
+	{
+		public MenuPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
